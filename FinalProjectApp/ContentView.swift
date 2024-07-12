8ac3c6c098b032effa258b72ac30613651a6614d
Kayla Tabalon <kaylatabalon@gmail.com>
2024-07-12 16:01:21 -0400

    Began with VStack

--- FinalProjectApp/ContentView.swift
+++ FinalProjectApp/ContentView.swift
@@ -10,12 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
         }
-        .padding()
     }
 }
 
