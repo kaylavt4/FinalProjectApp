383895b0f81ffab8c72d292293c499c87c9569c4
Kayla Tabalon <kaylatabalon@gmail.com>
2024-07-12 15:59:30 -0400

    Initial Commit

---
+++ FinalProjectApp/FinalProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectAppApp.swift
+//  FinalProjectApp
+//
+//  Created by Kayla Tabalon on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
