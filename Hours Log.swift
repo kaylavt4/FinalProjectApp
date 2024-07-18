39e4db0e533658fda6325ea78b529744520adcd2
Saanvi Mishra <saanmish26@gmail.com>
2024-07-18 14:18:33 -0400

    add the volunteer log page

---
+++ Hours Log.swift
@@ -0,0 +1,18 @@
+//
+//  Hours Log.swift
+//  FinalProjectApp
+//
+//  Created by Meenakshi Mishra on 7/18/24.
+//
+
+import SwiftUI
+
+struct Hours_Log: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hours_Log()
+}
